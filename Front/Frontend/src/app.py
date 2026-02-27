7aa5042b0963960d32cbe325804183d5f3a3c1df
dev-jesrod <rodolfolizr19@gmail.com>
2026-02-26 23:59:53 -0700

    Se añade 1 metodo(add) y 3 clases(Text, Row y Buttont)
    El metodo add se utiliza para añadir controles (widgets).
    
    La clase Text se utiliza para mostrar texto.
    
    La clase Row permite organizar controles hijos en una disposicion
    horizontal.
    
    La clase Button se utiliza para mostrar texto, iconos e interactuar con
    los controladores de eventos para la interaccion del usuario.
    
    Se añade un texto que muestra "Inventario"
    Se añaden Botones, por el momento no tiene interaccion con el usuario
    pero su funcion principal sera redirigir a una nueva ventana que
    visualizara los datos de los articulos.
    Por ultimo se organizan en 2 para una visualizacion diferente.

--- Front/Frontend/src/app.py
+++ Front/Frontend/src/app.py
@@ -5,6 +5,25 @@ def main( page: ft.Page ):
     
     #Titulo Interfaz
     page.title = "Interfaz Grafica"
-
+    
+    page.add( ft.Text("Inventario") )
+    
+    page.add(
+        
+        ft.Row(
+            controls = [
+                ft.Button("Procesadores"),
+                ft.Button("Memorias RAM")
+            ]
+        ),
+        ft.Row(
+            controls = [
+                ft.Button("Fuentes de Poder"),
+                ft.Button("Tarjetas Grafica")
+            ]
+        )
+        
+    )#fin-page.add
+   
 
 ft.run( main )
