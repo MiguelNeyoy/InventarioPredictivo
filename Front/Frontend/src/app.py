38a240f329d382d8e3989f82096d415df111c8d2
dev-jesrod <rodolfolizr19@gmail.com>
2026-02-27 00:34:00 -0700

    Se agrega la clase Colors.
    La clase Colors permite la estilizacion de la interfaz.
    
    Se agrego color y tamaño al texto "Inventario".
    Se cambia el color de todos los botones a Blanco.

--- Front/Frontend/src/app.py
+++ Front/Frontend/src/app.py
@@ -6,22 +6,22 @@ def main( page: ft.Page ):
     #Titulo Interfaz
     page.title = "Interfaz Grafica"
     
-    page.add( ft.Text("Inventario") )
+    page.add( ft.Text("Inventario", color = ft.Colors.BLUE, size = 28) )
     
     page.add(
         
         ft.Row(
             controls = [
-                ft.Button("Procesadores"),
-                ft.Button("Memorias RAM")
+                ft.Button("Procesadores", color = ft.Colors.WHITE ),
+                ft.Button("Memorias RAM", color = ft.Colors.WHITE )
             ]
-        ),
+        ),#fin-Row
         ft.Row(
             controls = [
-                ft.Button("Fuentes de Poder"),
-                ft.Button("Tarjetas Grafica")
+                ft.Button("Fuentes de Poder", color = ft.Colors.WHITE),
+                ft.Button("Tarjetas Grafica", color = ft.Colors.WHITE)
             ]
-        )
+        )#fin-Row
         
     )#fin-page.add
    
