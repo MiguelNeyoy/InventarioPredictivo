335b8980228097e789653dce2cfb447014aeff91
dev-jesrod <rodolfolizr19@gmail.com>
2026-03-02 22:40:33 -0700

    Se reorganiza estructura de page.add y se elimina la clase ft.Row
    Se reubica la posicion de las variables "titulo" y "txtDescripcion", ya
    no se encuentran arriba del page.add. Esto para tener una mejor
    visualizacion del codigo.
    
    Al tener problemas con el diseño del menu y entrando en una fase
    experimental. Se decidio borrar la clase Row, y añadimos la clase Column
    que almacera como columna a los botones, esto con el fin de seguir con
    el nuevo diseño experimental.

--- Front/Frontend/src/app.py
+++ Front/Frontend/src/app.py
@@ -7,29 +7,23 @@ def main( page: ft.Page ):
     page.bgcolor = "white"
 
     titulo:str = 'Gestor de Inventario'
-    page.add( ft.Text(titulo, color = ft.Colors.BLUE, size = 28) )
-    
-    page.add( ft.SearchBar(bar_hint_text="Buscar") )
-    
     txtDescripcion:str = 'Bienvenido a tu Gestor de Inventario Retail'
-    page.add( ft.Text(txtDescripcion, color = ft.Colors.BLACK) )
     
-    page.add(
-        
-        ft.Row(
-            controls = [
-                ft.Button("Procesadores", color = ft.Colors.WHITE ),
-                ft.Button("Memorias RAM", color = ft.Colors.WHITE )
-            ]
-        ),#fin-Row
-        ft.Row(
-            controls = [
-                ft.Button("Fuentes de Poder", color = ft.Colors.WHITE),
-                ft.Button("Tarjetas Grafica", color = ft.Colors.WHITE)
-            ]
-        )#fin-Row
-        
-    )#fin-page.add
+    page.add( ft.Text(titulo, color = ft.Colors.BLUE, size = 28) )
+    page.add( ft.Text(txtDescripcion, color = ft.Colors.BLACK) )
    
+    campos = ft.Column(
+        
+        controls = [
+            
+            ft.Button("Procesadores", color = ft.Colors.WHITE ),
+            ft.Button("Memorias RAM", color = ft.Colors.WHITE ),
+            ft.Button("Fuentes de Poder", color = ft.Colors.WHITE),
+            ft.Button("Tarjetas Grafica", color = ft.Colors.WHITE)
+        ]
+        
+    )
+    page.add(campos)
+
 
 ft.run( main )
