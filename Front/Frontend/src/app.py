e343c8a60d5cf7ed5cafec46f97d3c8dac1117f1
dev-jesrod <rodolfolizr19@gmail.com>
2026-03-04 22:59:37 -0700

    Implementacion del inventario y cambios en aspectos.
    Se añadio ligeros cambios para el diseño:
    Se añade ft.Divider para simular una linea de separacion debajo de
    pade.add.
    Se cambia la clase ft.Button por ft.ElevatedButton, debido a que ya esta
    quedando obsoleto en nuevas versiones de flet.
    
    Para poder integrar un inventario visual se utiliza por el momento
    ft.DataTable.
    
    ft.DataTable quedara guardada en una variable con nombre
    "tablaInventario".
    Dentro de la estructura de ft.DataTable se utilza columns y row.
    columsn utilza la clase ft.DataColumn que es el campo o encabezado de
    la tabla.
    row utilza la clase ft.DataRow que es el dato o valor que pertenecera al
    campo o encabezado de la tabla.

--- Front/Frontend/src/app.py
+++ Front/Frontend/src/app.py
@@ -10,20 +10,41 @@ def main( page: ft.Page ):
     txtDescripcion:str = 'Bienvenido a tu Gestor de Inventario Retail'
     
     page.add( ft.Text(titulo, color = ft.Colors.BLUE, size = 28) )
+    page.add( ft.Divider(thickness = 5, color = "black") )
     page.add( ft.Text(txtDescripcion, color = ft.Colors.BLACK) )
-   
+    page.add( ft.Divider(height = 40, opacity = 1) )
+    
     campos = ft.Column(
-        spacing = 30,
         controls = [
-            
-            ft.Button("Procesadores", color = ft.Colors.WHITE ),
-            ft.Button("Memorias RAM", color = ft.Colors.WHITE ),
-            ft.Button("Fuentes de Poder", color = ft.Colors.WHITE),
-            ft.Button("Tarjetas Grafica", color = ft.Colors.WHITE)
-        ]
-        
+            ft.ElevatedButton("Procesadores", color = "white", width = 150),
+            ft.ElevatedButton("Memorias RAM", color = "white", width = 150),
+            ft.ElevatedButton("Fuentes de Poder", color = "white", width = 150),
+            ft.ElevatedButton("Tarjetas Grafica", color = "white", width = 150),
+        ],
+        spacing = 30,
     )
     page.add(campos)
+    
+    tablaInventario = ft.DataTable(
+        columns = [
+            ft.DataColumn( ft.Text("Producto") ),
+            ft.DataColumn( ft.Text("Categoria") ),
+            ft.DataColumn( ft.Text("Precio") ),
+            ft.DataColumn( ft.Text("Stock") )
+        ],
+        rows = [
+            ft.DataRow(
+                cells = [
+                    ft.DataCell( ft.Text("AMD Ryzen 5 5600X") ),
+                    ft.DataCell( ft.Text("Procesador") ),
+                    ft.DataCell( ft.Text("3200") ),
+                    ft.DataCell( ft.Text("20") )
+                ]
+            )
+        ],
+    )
+    
+    page.add(tablaInventario)
 
 
 ft.run( main )
