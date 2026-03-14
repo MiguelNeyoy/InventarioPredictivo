f2e977caaee4930b64515df4e297efcd4a6da996
dev-jesrod <rodolfolizr19@gmail.com>
2026-03-14 12:16:02 -0700

    Eliminacion de Botones. Se elimino los botones de categoria ya que no cumplen con la vision del proyecto. Cambios: Al boton "Predecir Demanda" se le cambia el icono

--- Frontend/src/app.py
+++ Frontend/src/app.py
@@ -1,19 +1,6 @@
 import flet as ft
-import csv #  <-- Esta libreria permite leer los arvhicos csv.
 
 def main(page: ft.Page):
-    
-    '''
-    carpeta = ft.FilePicker()
-    page.overlay.append(carpeta)
-    
-    #Funcion para abrir el Explorardor de Archivos de Windows
-    async def abrirExploradorDeArchivos(e):
-        await carpeta.pick_files(
-            allow_multiple=False,
-            allowed_extensions= ["csv"]
-        )
-    '''
     
     # 1. Configuración principal de la página
     page.title = "Gestor de Inventario Predictivo"
@@ -35,16 +22,9 @@ def main(page: ft.Page):
     # 3. Menú Lateral (Sidebar)
     menu_botones = ft.Column(
         controls=[
-            ft.Text("Categorías", weight=ft.FontWeight.BOLD, color=ft.Colors.BLUE_GREY_700),
-            ft.Button("Procesadores", icon=ft.Icons.MEMORY, width=200, style=ft.ButtonStyle(shape=ft.RoundedRectangleBorder(radius=8))),
-            ft.Button("Memorias RAM", icon=ft.Icons.MEMORY, width=200, style=ft.ButtonStyle(shape=ft.RoundedRectangleBorder(radius=8))),
-            ft.Button("Fuentes de Poder", icon=ft.Icons.POWER, width=200, style=ft.ButtonStyle(shape=ft.RoundedRectangleBorder(radius=8))),
-            ft.Button("Tarjetas Gráficas", icon=ft.Icons.MONITOR, width=200, style=ft.ButtonStyle(shape=ft.RoundedRectangleBorder(radius=8))),
-            
-            ft.Divider(height=30), 
             
             ft.Text("Inteligencia Artificial", weight=ft.FontWeight.BOLD, color=ft.Colors.BLUE_GREY_700),
-            ft.Button("Predecir Demanda", icon=ft.Icons.AUTO_GRAPH, width=200, bgcolor=ft.Colors.BLUE_700, color=ft.Colors.WHITE),
+            ft.Button("Predecir Demanda", icon=ft.Icons.UPLOAD_FILE_OUTLINED, width=200, bgcolor=ft.Colors.BLUE_700, color=ft.Colors.WHITE),
         ],
         spacing=15,
     )
@@ -58,24 +38,7 @@ def main(page: ft.Page):
     )
     
     # 4. Tabla de Inventario
-    '''
-    filas = []
-    
-    with open('datosCSV.csv', 'r', encoding="utf-8") as inventario:
-        columna = csv.reader(inventario)
-    
-        for fila in columna:
-            filas.append(
-                ft.DataRow(
-                    cells = [
-                        ft.DataCell(ft.Text( fila[0] )),
-                        ft.DataCell(ft.Text( fila[1] )),
-                        ft.DataCell(ft.Text( fila[2] )),
-                        ft.DataCell(ft.Text( fila[3] ))
-                    ]
-                )
-            )    
-    '''
+        
     tabla_inventario = ft.DataTable(
         columns=[
             ft.DataColumn(ft.Text("Producto", weight=ft.FontWeight.BOLD)),
