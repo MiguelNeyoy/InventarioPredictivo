b5b08e52f6c57c1733a6aab8105d6e1557e8d23b
MiguelNeyoy <miguelneyoycn@gmail.com>
2026-05-02 10:05:18 -0700

    FIX/ Correccion de parametros ahora es vairable enfuncion los dias y el porcentanje de siniestros

--- Back/predictor.py
+++ Back/predictor.py
@@ -67,11 +67,9 @@ class MotorInventario:
 
         return pd.DataFrame(resultados)
 
-    def calcular_metricas(self, dias_test=15):
-        """
-        Calcula MAE y RMSE apartando los últimos dias_test para validación.
-        """
+    def generar_prediccion_y_metricas(self, start_date, end_date, dias_test=15):
         resultados = []
+        metricas = []
         lista_productos = self.df_historico["Producto"].unique()
 
         for articulo in lista_productos:
@@ -80,26 +78,81 @@ class MotorInventario:
             ].copy()
             df_articulo = df_articulo.sort_values("ds")
 
-            if len(df_articulo) <= dias_test:
-                continue
-
-            df_entrenamiento = df_articulo.iloc[:-dias_test]
-            df_test = df_articulo.iloc[-dias_test:]
-
             modelo = Prophet(holidays=self.feriados)
-            modelo.fit(df_entrenamiento[["ds", "y"]])
+            modelo.fit(df_articulo[["ds", "y"]])
 
-            futuro = df_test[["ds"]]
+            futuro = pd.DataFrame(
+                {"ds": pd.date_range(start=start_date, end=end_date, freq="D")}
+            )
             prediccion = modelo.predict(futuro)
-
-            y_real = df_test["y"].values
-            y_predicho = prediccion["yhat"].values
-
-            mae = mean_absolute_error(y_real, y_predicho)
-            rmse = np.sqrt(mean_squared_error(y_real, y_predicho))
+            total_estimado = prediccion["yhat"].sum()
 
             resultados.append(
-                {"Producto": articulo, "MAE": round(mae, 2), "RMSE": round(rmse, 2)}
+                {
+                    "Producto": articulo,
+                    "Venta_Estimada": round(max(0, total_estimado)),
+                }
             )
 
-        return pd.DataFrame(resultados)
+            if dias_test >= 5 and len(df_articulo) > dias_test:
+                df_entrenamiento = df_articulo.iloc[:-dias_test]
+                df_test = df_articulo.iloc[-dias_test:]
+
+                modelo_test = Prophet(holidays=self.feriados)
+                modelo_test.fit(df_entrenamiento[["ds", "y"]])
+                prediccion_test = modelo_test.predict(df_test[["ds"]])
+
+                y_real = df_test["y"].values
+                y_predicho = prediccion_test["yhat"].values
+
+                mae = mean_absolute_error(y_real, y_predicho)
+                rmse = np.sqrt(mean_squared_error(y_real, y_predicho))
+
+                metricas.append(
+                    {
+                        "Producto": articulo,
+                        "MAE": round(mae, 2),
+                        "RMSE": round(rmse, 2),
+                    }
+                )
+
+        return pd.DataFrame(resultados), pd.DataFrame(
+            metricas, columns=["Producto", "MAE", "RMSE"]
+        )
+
+    # def calcular_metricas(self, dias_test=15):
+    #     """
+    #     Calcula MAE y RMSE apartando los últimos dias_test para validación.
+    #     """
+    #     resultados = []
+    #     lista_productos = self.df_historico["Producto"].unique()
+
+    #     for articulo in lista_productos:
+    #         df_articulo = self.df_historico[
+    #             self.df_historico["Producto"] == articulo
+    #         ].copy()
+    #         df_articulo = df_articulo.sort_values("ds")
+
+    #         if len(df_articulo) <= dias_test:
+    #             continue
+
+    #         df_entrenamiento = df_articulo.iloc[:-dias_test]
+    #         df_test = df_articulo.iloc[-dias_test:]
+
+    #         modelo = Prophet(holidays=self.feriados)
+    #         modelo.fit(df_entrenamiento[["ds", "y"]])
+
+    #         futuro = df_test[["ds"]]
+    #         prediccion = modelo.predict(futuro)
+
+    #         y_real = df_test["y"].values
+    #         y_predicho = prediccion["yhat"].values
+
+    #         mae = mean_absolute_error(y_real, y_predicho)
+    #         rmse = np.sqrt(mean_squared_error(y_real, y_predicho))
+
+    #         resultados.append(
+    #             {"Producto": articulo, "MAE": round(mae, 2), "RMSE": round(rmse, 2)}
+    #         )
+
+    #     return pd.DataFrame(resultados)
