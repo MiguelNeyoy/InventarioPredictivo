f9982bc4643dd78789f6b8b2d5e78ec33e6e1293
dev-jesrod <rodolfolizr19@gmail.com>
2026-05-05 11:03:24 -0700

    REFACTOR: Añadiendo nueva funcion en file_picker
    Antecedentes:
    Se utilizaba la libreria nativa de pyhton tkinter para abrir el
    explorador de archivos, dejando de lado la potencia e integracion del
    framework Flet. Añadiendo mas codigo e innecesario.
    
    Solucion:
    Utilizamos las funciones predeterminadas del framework para abrir el
    explorador de archivos.
    Utiliazmos una variable  que permite redirigirte sobre que carperta desees
    abrir(initial_directory).

--- Frontend/src/views/dashboard.py
+++ Frontend/src/views/dashboard.py
@@ -156,7 +156,8 @@ def crear_vista_dashboard(page: ft.Page):
         # El resultado se obtiene directamente del método await
         await file_picker.pick_files(
             dialog_title= "Seleccionar archivo de ventas",
-            allowed_extensions= ["Archivos CSV","*csv"]
+            allowed_extensions= ["Archivos CSV","*csv"],
+            initial_directory= _back_path
         )
     
     def abrir_dialogo_stock(e):
